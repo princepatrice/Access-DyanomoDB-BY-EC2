21f20e9217d233920f211cfd0baa36b77c63ae18
User <dzogbemapatrice@gmail.com>
2023-07-11 16:29:47 -0500

    init aproject

---
+++ readme.txt
@@ -0,0 +1,17 @@
+1. Connect to your Amazon Linux 2 instance using SSH.
+
+2. Update the package manager's cache:
+   sudo yum update
+
+3. Install the necessary dependencies for building Node.js from source:
+   sudo yum install -y gcc-c++ make
+
+4. Download the Node.js binary distribution for Linux:
+   curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
+
+5. Install Node.js:
+   sudo yum install -y nodejs
+
+6. Verify the installation:
+   node -v
+   npm -v
---
+++ server.js
@@ -0,0 +1,62 @@
+require("dotenv").config()
+const os = require('os');
+
+const express = require('express');
+const AWS = require('aws-sdk');
+const app = express();
+const port = process.env.PORT;
+
+// Configure AWS SDK with your AWS credentials and region
+AWS.config.update({ region: process.env.YOUR_AWS_REGION });
+
+// Create an instance of the DynamoDB service
+const dynamodb = new AWS.DynamoDB();
+
+// Define the name of the DynamoDB table
+const tableName = 'Counter';
+
+app.get('/', (req, res) => {
+  const serverIp = os.hostname();
+  // Increment the counter value in DynamoDB
+  incrementCounter(serverIp)
+    .then(() => {
+      res.send('Counter value updated successfully!');
+    })
+    .catch((error) => {
+      console.error('Error updating counter:', error);
+      res.status(500).send('An error occurred while updating the counter.');
+    });
+});
+
+// Function to increment the counter in DynamoDB
+function incrementCounter(serverIp) {
+  const params = {
+    TableName: tableName,
+    Key: { serverIp: { S: serverIp } },
+    UpdateExpression: 'ADD #val :incr',
+    ExpressionAttributeNames: { '#val': 'value' },
+    ExpressionAttributeValues: { ':incr': { N: '1' } },
+    ReturnValues: 'UPDATED_NEW',
+  };
+
+  return new Promise((resolve, reject) => {
+    dynamodb.updateItem(params, (error, data) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+
+
+
+
+
